7dc225b414faece9efd03f9652be6c355f3d1f72
Jesus Cruz Manjavacas <143967655+jecruman@users.noreply.github.com>
2025-11-13 22:37:53 +0100

    Update Section.tsx

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,1 +1,11 @@
-export default function S({children}:{children:any}){return <section>{children}</section>}
+import { clsx } from 'clsx';
+
+export default function Section({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return <section className={clsx('w-full', className)}>{children}</section>;
+}
