12a640ffd0a6b701e3e9b77f4fc591ce2e98cadd
Jesus Cruz Manjavacas <143967655+jecruman@users.noreply.github.com>
2025-11-13 22:37:31 +0100

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,1 +1,15 @@
-export default function C({children}:{children:any}){return <div>{children}</div>}
+import { clsx } from 'clsx';
+
+export default function Container({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={clsx('mx-auto max-w-5xl px-4', className)}>
+      {children}
+    </div>
+  );
+}
