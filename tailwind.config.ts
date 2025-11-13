751342c1ff0f9bfb2e9ae99bc1dd5d985282e69f
Jesus Cruz Manjavacas <143967655+jecruman@users.noreply.github.com>
2025-11-13 21:15:35 +0100

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from 'tailwindcss';
-export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: { colors: { brand: { green: '#2ECC71', dark: '#2E2E2E' } } } },
-  plugins: [],
-} satisfies Config;
