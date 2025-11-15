dcea1b419e865b513f0960db0ba21c411e34ec5f
Jesus Cruz Manjavacas <143967655+jecruman@users.noreply.github.com>
2025-11-15 18:13:33 +0100

    Update tailwind.config.ts - saas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'tailwindcss';
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+  './src/app/**/*.{js,ts,jsx,tsx}',
+  './src/components/**/*.{js,ts,jsx,tsx}',
+  './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: { extend: { colors: { brand: { green: '#2ECC71', dark: '#2E2E2E' } } } },
   plugins: [],
 } satisfies Config;
